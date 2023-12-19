d1a11f0fb5d3eaa9e3b69bed983ae997310ab904
nadiia1994 <ms2.foteniuk@gmail.com>
2023-12-19 03:01:42 +0000

    class1 files

---
+++ Class1/bucket.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "hello" {
+  bucket_prefix = "kaizen-"
+
+}
+
+resource "aws_s3_bucket" "hello2" {
+           bucket = "kaizen-hello111"
+
+}
+
+       output bucket {
+    value =aws_s3_bucket.hello2.arn
+}
+
+output bucket2 {
+    value =aws_s3_bucket.hello.arn
+}
